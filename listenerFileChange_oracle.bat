4cb5e80e894d9515c1ac8cbfa0a6ff64842c5d7e
bssmMrchoi <93627909+bssmMrchoi@users.noreply.github.com>
2022-05-13 10:53:18 +0900

    programming1 class setting
    programming1 class setting

---
+++ listenerFileChange_oracle.bat
@@ -0,0 +1,10 @@
+@echo off
+
+for /f "delims=" %%i in (C:\Users\June\Desktop\listener.ora) do (
+	echo %%i | find "st01">nul
+	if errorlevel 1 (echo %%i >> C:\oraclexe\app\oracle\product\11.2.0\server\network\ADMIN\listener2.ora) else (	echo ^(ADDRESS ^= ^(PROTOCOL ^= TCP^)^(HOST ^= %COMPUTERNAME%^)^(PORT ^= 1521^)^) >>C:\oraclexe\app\oracle\product\11.2.0\server\network\ADMIN\listener2.ora)
+)
+del C:\oraclexe\app\oracle\product\11.2.0\server\network\ADMIN\listener.ora
+ren C:\oraclexe\app\oracle\product\11.2.0\server\network\ADMIN\listener2.ora listener.ora
+net start "OracleXETNSListener"
+pause
