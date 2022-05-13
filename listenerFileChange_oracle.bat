113e3b800b9b79a473fc5c9044b46d29e484e9cf
bssmMrchoi <93627909+bssmMrchoi@users.noreply.github.com>
2022-05-13 10:58:07 +0900

    Update listenerFileChange_oracle.bat

--- listenerFileChange_oracle.bat
+++ listenerFileChange_oracle.bat
@@ -1,6 +1,6 @@
 @echo off
 
-for /f "delims=" %%i in (C:\Users\June\Desktop\listener.ora) do (
+for /f "delims=" %%i in (C:\oraclexe\app\oracle\product\11.2.0\server\network\ADMIN\listener.ora) do (
 	echo %%i | find "st01">nul
 	if errorlevel 1 (echo %%i >> C:\oraclexe\app\oracle\product\11.2.0\server\network\ADMIN\listener2.ora) else (	echo ^(ADDRESS ^= ^(PROTOCOL ^= TCP^)^(HOST ^= %COMPUTERNAME%^)^(PORT ^= 1521^)^) >>C:\oraclexe\app\oracle\product\11.2.0\server\network\ADMIN\listener2.ora)
 )
